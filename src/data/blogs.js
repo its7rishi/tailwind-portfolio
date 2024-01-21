279eba84bcc197666c77af008c3679ce375c5ee0
Saptarshi Majumdar <saptarshi.maj@yahoo.com>
2024-01-21 19:04:59 +0530

    updated image paths

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -2,7 +2,7 @@ const BlogList = [
   {
     id: 1,
     url: "https://frontendfan.blogspot.com/2023/10/get-to-know-recursion-in-javascript.html",
-    image: "/src/assets/images/blog-recursion.png",
+    image: "/assets/images/blog-recursion.png",
     title: "Get To Know Recursion in JavaScript",
     description:
       "A blog explaining how recursion works in JavaScript with simple examples.",
