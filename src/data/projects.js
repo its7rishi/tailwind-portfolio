d6d738169a866e54a834d527829e96964544b6e9
Saptarshi Majumdar <saptarshi.maj@yahoo.com>
2024-01-22 18:13:41 +0530

    modified project image path

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ const ProjectList = [
     id: 1,
     title: "Space Tourism",
     url: "https://incomparable-buttercream-862782.netlify.app/",
-    image: "/src/assets/images/space-tourism.png",
+    image: "/assets/images/space-tourism.png",
     description:
       "A space tourism webpage providing information about destinations like Moon,  Mars, Europa and Titan.",
   },
@@ -11,7 +11,7 @@ const ProjectList = [
     id: 2,
     title: "Countries API",
     url: "https://courageous-biscochitos-c5c8d8.netlify.app/",
-    image: "/src/assets/images/countries-api.png",
+    image: "/assets/images/countries-api.png",
     description:
       "A project providing population, capital, currency used, domain names, etc. by searched countries.",
   },
@@ -19,7 +19,7 @@ const ProjectList = [
     id: 3,
     title: "MeaningFul Dictionary",
     url: "https://main--benevolent-fairy-6316d5.netlify.app/",
-    image: "/src/assets/images/meaningful-dictionary.png",
+    image: "/assets/images/meaningful-dictionary.png",
     description:
       "A dictionary application providing definition, examples, synonyms and antonyms of searched words.",
   },
@@ -27,7 +27,7 @@ const ProjectList = [
     id: 4,
     title: "Tic Tac Toe",
     url: "https://javascript-tic-tac-toe-murex.vercel.app/",
-    image: "/src/assets/images/tic-tac-toe.png",
+    image: "/assets/images/tic-tac-toe.png",
     description:
       "Tic Tac Toe game application. Users can play against the computer. Made with HTML, CSS and Vanilla JS.",
   },
@@ -35,7 +35,7 @@ const ProjectList = [
     id: 5,
     title: "Chat App - CSS Illustration",
     url: "https://brilliant-kulfi-1100db.netlify.app/",
-    image: "/src/assets/images/chat-app-illustration.png",
+    image: "/assets/images/chat-app-illustration.png",
     description:
       "CSS illustration of a chat application using tailwind css. Showcasing a chat app in mobile interface.",
   },
@@ -43,7 +43,7 @@ const ProjectList = [
     id: 6,
     title: "Testimonials Grid",
     url: "https://graceful-lollipop-9d5454.netlify.app/",
-    image: "/src/assets/images/testimonials-grid.png",
+    image: "/assets/images/testimonials-grid.png",
     description:
       "A customer testimonials grid design showcasing the CSS Grid functionality. Made using TailwindCSS.",
   },
